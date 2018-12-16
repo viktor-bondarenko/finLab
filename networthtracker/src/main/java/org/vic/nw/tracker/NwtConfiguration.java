4273234b70263d30680e9387ba23da0c15c2afa9
Viktor Bondarenko <viktor.bondarenko@globallogic.com>
2018-12-16 09:12:30 -0800

    Rest controller draft

--- networthtracker/src/main/java/org/vic/nw/tracker/NwtConfiguration.java
+++ networthtracker/src/main/java/org/vic/nw/tracker/NwtConfiguration.java
@@ -5,18 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.vic.nw.tracker.repository.JsonDao;
 
 import java.util.TimeZone;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = {"org.vic.nw.tracker"})
 public class NwtConfiguration {
-
-    @Bean
-    public JsonDao jsonDao(){
-        return new JsonDao();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
---
+++ networthtracker/src/main/java/org/vic/nw/tracker/controller/RecordController.java
@@ -0,0 +1,7 @@
+package org.vic.nw.tracker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecordController {
+}
